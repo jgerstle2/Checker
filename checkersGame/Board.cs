e5f917e6216498622fb092eebe2bc4c18a46db2b
jennygerstle@gmail.com <jennygerstle@gmail.com>
2021-10-26 20:34:16 -0400

    added peices to the board

---
+++ checkersGame/Board.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Checkers
+{
+    public partial class Board : Form
+    {
+        private enum Piece
+        {
+            EMPTY, WHITE_P, BLACK_P, WHITE_K, BLACK_K
+        }
+
+        private class Position
+        {
+            private Piece[,] position;
+            public Position(int size)
+            {
+                position = new Piece[size, size];
+                for (int row = 0; row < size; ++row)
+                {
+                    for (int col = 0; col < size; ++col)
+                    {
+                        int parity = (row + col) % 2;
+                        if (parity == 0)
+                        {
+                            position[row, col] = Piece.EMPTY;
+                        }
+                        else
+                        {
+                            if (row < 3);
+                        }}}}}}}
